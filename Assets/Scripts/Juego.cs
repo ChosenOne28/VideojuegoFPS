a725f841c47fa6cfaef2dde526e15274ee5d1ed2
Francisco Junior Puente <106897105+ChosenOne28@users.noreply.github.com>
2023-03-07 12:03:43 -0400

    Add files via upload

---
+++ Assets/Scripts/Juego.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Juego : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Empezar()
+	{
+		SceneManager.LoadScene("ShootCamp");
+	}
+
+	public void veropciones(){
+		SceneManager.LoadScene("Opciones");
+	}
+
+	public void EscenaPrincipal()
+	{
+		SceneManager.LoadScene("MenuPrincipal");
+	}
+
+	public void Salir()
+	{
+		Application.Quit();
+	}
+
+}
